5882feefdbee7340f06290be3d16622a6c5dca47
tuvshoo <mac@macs-MacBook.local>
2018-01-16 07:21:20 +0800

    About mongolia, travel pdf

--- resources/assets/js/env.js
+++ resources/assets/js/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-	API_KEY: "http://www.mongolianblackfish.com/",
-	//API_KEY: "http://localhost:8000/",
+	//API_KEY: "http://www.mongolianblackfish.com/",
+	API_KEY: "http://localhost:8000/",
 	DEBUG: true,
 }
--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -9,7 +9,7 @@
     			<h1 class="white text-center">Our team</h1>
 				</br>
     			<h4 class="white" style="line-height: 1.5em;">
-    				We’ve been fly-fishing for Mongolian taimen longer than just about anyone.  Our founders started fly-fishing in Mongolia  back in 1991.  We have dozens of boats.  Our remote fly-fishing camps provide access to hundreds of miles of Mongolian rivers.  There is no one better staffed and equipped for this kind of trip.
+    				Since 2008, some family friends gathered and collaborates, making round trip through Mongolian territory and fishing. Since 2015, the friends are working for tourism industry and especially working on developing fish tour. Our company not only running fish tour, but we are contributing and starting in campaign since 2017, which preserving and breeding this endangered –fish kind. In the scope of this campaign, we working on the exploration, which shows Taimen fish’s distribution of Mongolian territory, water and regional aspects. 
     			</h4>
 	    		</br>
 	    		</br>
@@ -19,7 +19,25 @@
 			</form>
 			</br>
 			</br>
-			</br>
+            </br>
+            <h1 class="white text-center" style="margin-bottom: 80px; margin-top: 120px;">About Mongolia</h1>
+            <object data="/pdf/About Mongolia.pdf" type="application/pdf" width="100%" height="700">
+                <iframe src="/pdf/About Mongolia.pdf" width="100%" height="100%" style="border: none;">
+                This browser does not support PDFs. Please download the PDF to view it: <a href="/pdf/sample-3pp.pdf">Download PDF</a>
+                </iframe>
+            </object>
+            <h1 class="white text-center" style="margin-bottom: 80px; margin-top: 120px;">About Khuvsgul</h1>
+            <object data="/pdf/Intro of Khuvsgul.pdf" type="application/pdf" width="100%" height="700">
+                <iframe src="/pdf/Intro of Khuvsgul.pdf" width="100%" height="100%" style="border: none;">
+                This browser does not support PDFs. Please download the PDF to view it: <a href="/pdf/sample-3pp.pdf">Download PDF</a>
+                </iframe>
+            </object>
+            <h1 class="white text-center" style="margin-bottom: 80px; margin-top: 120px;">Visa Information</h1>
+            <object data="/pdf/Visa information.pdf" type="application/pdf" width="100%" height="700">
+                <iframe src="/pdf/Visa information.pdf" width="100%" height="100%" style="border: none;">
+                This browser does not support PDFs. Please download the PDF to view it: <a href="/pdf/sample-3pp.pdf">Download PDF</a>
+                </iframe>
+            </object>
     	</div>
     </div>
 @stop
